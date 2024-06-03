650e4431692413ca2fedad3a2184816d76e47b4c
Juboraj Sujon <mdsujon72@gmail.com>
2024-06-03 23:47:19 +0600

    added button

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,18 @@ import "react-toastify/dist/ReactToastify.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./AuthProvider/AuthProvider";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
+    <AuthProvider>
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ToastContainer />
+        <HelmetProvider>
+          <RouterProvider router={router} />
+          <ToastContainer />
+        </HelmetProvider>
       </QueryClientProvider>
-    </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
