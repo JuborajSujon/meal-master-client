450ae8131d9bd6965990ac047f82406689c4164b
Juboraj Sujon <mdsujon72@gmail.com>
2024-06-03 21:40:24 +0600

    Added firebase config file

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,16 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
-      <RouterProvider router={router} />
-      <ToastContainer />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ToastContainer />
+      </QueryClientProvider>
     </HelmetProvider>
   </React.StrictMode>
 );
