1ee915313801ef40fbd70a7ddaad6a3d7dc20def
Juboraj Sujon <mdsujon72@gmail.com>
2024-06-10 04:22:35 +0600

    Added all review page function

--- src/pages/Dashboard/AdminPage/AllReviews/AllReviews.jsx
+++ src/pages/Dashboard/AdminPage/AllReviews/AllReviews.jsx
@@ -3,27 +3,50 @@ import Breadcrumbs from "../../../../components/Breadcrumbs/Breadcrumbs";
 import SectionTitle from "../../../../components/SectionTitle/SectionTitle";
 import { BiSolidDetail } from "react-icons/bi";
 import { MdDeleteForever } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import useAllReviews from "../../../../hooks/useAllReviews";
+import Loading from "../../../../components/Loading/Loading";
 
 function AllReviews() {
+  const [AllReviews, isLoading, refetch] = useAllReviews();
+  const axiosSecure = useAxiosSecure();
+
+  const handleDeleteReview = async (createdAt) => {
+    try {
+      await axiosSecure.delete(`/review/${createdAt}`);
+      refetch();
+
+      toast.success("Review deleted successfully", {
+        autoClose: 1500,
+      });
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <div>
       <Helmet>
-        <title>All Reviews | Dashboard</title>
+        <title>User Reviews | Dashboard</title>
       </Helmet>
       <div className="py-2">
         <Breadcrumbs />
       </div>
       <div>
-        <SectionTitle title="Manage All Reviews" />
+        <SectionTitle title="Reviews all Meals" />
       </div>
-      <div className="bg-orange-50 dark:text-slate-800 dark:bg-slate-800 p-4 rounded-md">
-        {/* table */}
+      <div className="bg-orange-50 dark:bg-slate-800 p-4 rounded-md">
         <div className="container p-2 mx-auto sm:p-4 text-gray-800">
           <div className="overflow-x-auto">
-            <table className="min-w-full text-xs">
+            <table className="min-w-full text-sm">
               <colgroup>
-                <col className="min-w-96" />
-                <col />
                 <col />
                 <col />
                 <col />
@@ -31,46 +54,47 @@ function AllReviews() {
               </colgroup>
               <thead className="bg-gray-300">
                 <tr className="text-left">
-                  <th className="p-3">Review</th>
+                  <th className="p-3">Reviews</th>
                   <th className="p-3">Meal Name</th>
-                  <th className="p-3">Like</th>
+                  <th className="p-3">Likes</th>
                   <th className="p-3">Review count</th>
-                  <th className="p-3 ">Action</th>
+                  <th className="p-3">Action</th>
                 </tr>
               </thead>
               <tbody>
-                <tr className="border-b border-opacity-20 border-gray-300 bg-gray-50">
-                  <td className="p-3">
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Impedit maiores soluta error et aliquid dolor obcaecati
-                      debitis placeat, deserunt ducimus.
-                    </p>
-                  </td>
-                  <td className="p-3">
-                    <p>Meal Name</p>
-                  </td>
-                  <td className="p-3">
-                    <p>200</p>
-                  </td>
-                  <td className="p-3">
-                    <p>200</p>
-                  </td>
+                {AllReviews?.map((review, index) => (
+                  <tr
+                    key={index}
+                    className="border-b border-opacity-20 border-gray-300 bg-gray-50">
+                    <td className="p-3">
+                      <p>{review?.reviews?.review}</p>
+                    </td>
+                    <td className="p-3">
+                      <p>{review?.meal_title}</p>
+                    </td>
+                    <td className="p-3">
+                      <p>{review?.likes_count}</p>
+                    </td>
+                    <td className="p-3">
+                      <p>{review?.rating?.reviewCount}</p>
+                    </td>
+                    <td className="p-3 flex justify-center items-center gap-2">
+                      <Link to={`/meal-details/${review?._id}`}>
+                        <button className="px-3 py-1 rounded-md bg-amber-600 text-gray-50">
+                          <BiSolidDetail size={16} />
+                        </button>
+                      </Link>
 
-                  <td className="p-3 flex items-center gap-2">
-                    <button
-                      data-tip="Meal View"
-                      className="px-3 py-1 tooltip rounded-md bg-amber-600 text-gray-50">
-                      <BiSolidDetail size={16} />
-                    </button>
-
-                    <button
-                      data-tip="Delete"
-                      className="px-3 py-1 tooltip rounded-md bg-red-600 text-gray-50">
-                      <MdDeleteForever size={16} />
-                    </button>
-                  </td>
-                </tr>
+                      <button
+                        onClick={() =>
+                          handleDeleteReview(review?.reviews?.created_time)
+                        }
+                        className="px-3 py-1  rounded-md bg-red-600 text-gray-50">
+                        <MdDeleteForever size={16} />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
