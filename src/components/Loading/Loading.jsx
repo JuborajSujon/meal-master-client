2cae6007dca883a406ed715f02f20bff77aad95d
Juboraj Sujon <mdsujon72@gmail.com>
2024-11-25 00:08:24 +0600

    fix loading width

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="text-center  absolute inset-0 bg-white/50 h-screen max-w-[1540px] flex justify-center items-center p-5">
+    <div className="text-center  absolute inset-0 bg-white/50 h-screen w-full flex justify-center items-center p-5">
       <span className="loading loading-spinner w-32"></span>
     </div>
   );
