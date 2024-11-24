240d912174e4c71c14d7eb057261d1df4419f6fc
Juboraj Sujon <mdsujon72@gmail.com>
2024-11-24 22:50:49 +0600

    upgrade bg and text color based on theme

--- src/components/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -13,7 +13,9 @@ export default function Breadcrumbs() {
     .map((crumb, index) => {
       currentLink += `/${crumb}`;
       return (
-        <ul key={index} className="crumb *:text-sm">
+        <ul
+          key={index}
+          className="crumb *:text-sm *:text-slate-600 *:dark:text-white">
           <li>
             <Link to={currentLink}>{crumb}</Link>
           </li>
@@ -28,11 +30,11 @@ export default function Breadcrumbs() {
           <Link
             to="/"
             className="flex items-center gap-2 hover:underline text-sm">
-            <FaHome className="text-slate-600 dark:text-slate-300" />
-            Home
+            <FaHome className="text-blue-600 dark:text-white" />
+            <span className="text-slate-600 dark:text-white">Home</span>
             <IoIosArrowForward
               size={16}
-              className="text-slate-600 dark:text-slate-300"
+              className="text-slate-600 dark:text-white"
             />
           </Link>
         </li>
