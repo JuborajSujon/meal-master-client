3683dbfdbb4d76b2865e628579ad2d640fd2a361
Juboraj Sujon <mdsujon72@gmail.com>
2024-11-25 19:08:31 +0600

    fix text color and size for responsive

--- src/pages/Home/HomeAboutUs/HomeAboutUs.jsx
+++ src/pages/Home/HomeAboutUs/HomeAboutUs.jsx
@@ -39,7 +39,7 @@ export default function HomeAboutUs() {
             subTitle="Where quality food meet satisfaction"
           />
 
-          <p className="text-lg text-slate-800 font-semibold px-4 mt-6">
+          <p className="text-sm sm:text-lg text-slate-800 dark:text-slate-300 font-semibold px-4 mt-6">
             It’s the perfect dining experience where every dish is crafted with
             fresh, high-quality ingredients and served by friendly staff who go.
           </p>
@@ -56,7 +56,7 @@ export default function HomeAboutUs() {
                   }}
                 />
                 <h2 className="card-title mt-10">Fast Food</h2>
-                <p className="">
+                <p className="text-sm sm:text-base">
                   Quick, delicious fast food for satisfying meals.
                 </p>
               </div>
@@ -72,7 +72,9 @@ export default function HomeAboutUs() {
                   }}
                 />
                 <h2 className="card-title">Healthy Food</h2>
-                <p>Lightning-fast delivery to your doorstep.</p>
+                <p className="text-sm sm:text-base">
+                  Lightning-fast delivery to your doorstep.
+                </p>
               </div>
             </div>
             <div className="card w-56 bg-white dark:bg-slate-800 text-slate-800 dark:text-slate-300 shadow-xl dark:shadow-gray-700">
@@ -86,7 +88,9 @@ export default function HomeAboutUs() {
                   }}
                 />
                 <h2 className="card-title">Fast Delivery</h2>
-                <p>Nutritious, tasty meals promoting a healthy lifestyle.</p>
+                <p className="text-sm sm:text-base">
+                  Nutritious, tasty meals promoting a healthy lifestyle.
+                </p>
               </div>
             </div>
           </div>
